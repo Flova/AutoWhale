51a4552433a1901a397cfe8d1851e589f99ab17c
Florian Vahl <florian@flova.de>
2020-02-13 14:54:20 +0100

    Add login command

---
+++ login.sh
@@ -0,0 +1,2 @@
+curl 'https://landing.mobyklick.de/index.php' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' -H 'Referer: https://landing.mobyklick.de/landingpage/' -H 'Upgrade-Insecure-Requests: 1' --data 'login=oneclick'
+
